f212d174a1df2947201b7c99e5209bf3c3a1b8a9
D4v1d0202 <dmlazzarini02@gmail.com>
2025-03-17 21:01:59 +0100

    mapbox implemented!!!!!!!

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ const App = () => {
     >
       <TileLayer
         attribution='Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
-        url={`https://api.mapbox.com/styles/v1/${VITE_USERNAME}/${VITE_STYLE_ID}/tiles/256/{z}/{x}/{y}@2x?access_token=${VITE_ACCESS_TOKEN}`}
-      />
+        url={`https://api.mapbox.com/styles/v1/${import.meta.env.VITE_USERNAME}/${import.meta.env.VITE_STYLE_ID}/tiles/{z}/{x}/{y}?access_token=${import.meta.env.VITE_ACCESS_TOKEN}`}
+/>
       <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
